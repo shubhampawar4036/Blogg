679b0cf611920672f417baae2808b6a1ae06397f
Shubham <shubhampawar4036@gmail.com>
2024-06-15 18:26:30 +0530

    client side completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {Route, Routes} from 'react-router-dom'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import PostDetails from './pages/PostDetails'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import Profile from './pages/Profile'
+import {  UserContextProvider } from './context/UserContext'
+import MyBlogs from './pages/MyBlogs'
 
+
+
+const App = () => {
+
+
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <UserContextProvider>
+      <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/login" element={<Login/>}/>
+      <Route exact path="/register" element={<Register/>}/>
+      <Route exact path="/write" element={<CreatePost/>}/>
+      <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
+      <Route exact path="/edit/:id" element={<EditPost/>}/>
+      <Route exact path="/myblogs/:id" element={<MyBlogs/>}/>
+      <Route exact path="/profile/:id" element={<Profile/>}/>
+      </Routes>
+    
+      </UserContextProvider>
   )
 }
 
