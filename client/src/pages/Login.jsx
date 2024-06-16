0f75dd813fb7725a98461d3cf72a66c24a1f18a7
Shubham Pawar <pawarshubham4036@gmail.com>
2024-06-16 13:38:40 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
 
   const handleLogin=async()=>{
     try{
-      const res=await axios.post(URL+"/api/auth/login",{email,password},{withCredentials:true})
+      const res=await axios.post("https://your-backend.vercel.app"+"/api/auth/login",{email,password},{withCredentials:true})
       // console.log(res.data)
       setUser(res.data)
       navigate("/")
